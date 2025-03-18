268d39200f044969463ca31cd090aaf56c290be1
DwiWahyu20 <146156027+DwiWahyu20@users.noreply.github.com>
2025-03-18 21:19:53 +0000

    rei

--- .wasp/out/sdk/wasp/dist/src/tasks/actions.d.ts
+++
@@ -1,8 +0,0 @@
-import { type Task } from "wasp/entities";
-import { type CreateTask, type UpdateTask, type DeleteTasks } from "wasp/server/operations";
-type CreateArgs = Pick<Task, "description">;
-export declare const createTask: CreateTask<CreateArgs, Task>;
-type UpdateArgs = Pick<Task, "id" | "isDone">;
-export declare const updateTask: UpdateTask<UpdateArgs>;
-export declare const deleteTasks: DeleteTasks<Task["id"][]>;
-export {};
