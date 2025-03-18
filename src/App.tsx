49aff71c5e4aab686fd22e3d251b71b6b9c794ca
DwiWahyu20 <146156027+DwiWahyu20@users.noreply.github.com>
2025-03-18 08:04:47 +0000

    router

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,39 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomeView from './views/HomeView';
+import AboutView from './views/AboutView';
 
-function App() {
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.tsx</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomeView />} />
+        <Route path="/about" element={<AboutView />} />
+      </Routes>
+    </Router>
   );
 }
-
 export default App;
---
+++ src/views/AboutView.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../components/Header';
+
+const AboutView: React.FC = () => {
+  return (
+    <div>
+      <Header title="About" />
+      <h1>About Us</h1>
+    </div>
+  );
+}
+
+export default AboutView;
