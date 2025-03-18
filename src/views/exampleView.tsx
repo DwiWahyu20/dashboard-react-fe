2afb5e19f6527a53cc4f95705fa829ecf7f1ec4c
DwiWahyu20 <146156027+DwiWahyu20@users.noreply.github.com>
2025-03-18 10:33:03 +0000

    bac

--- src/views/exampleView.tsx
+++ src/views/exampleView.tsx
@@ -1,27 +1,27 @@
-import React from 'react';
-// import React, { useEffect, useState } from 'react';
-// import { fetchExampleData } from '../controllers/exampleController';
+// import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { fetchExampleData } from '../controllers/exampleController';
 
 const ExampleView: React.FC = () => {
-  // const [data, setData] = useState<any>(null);
+  const [data, setData] = useState<any>(null);
 
-  // useEffect(() => {
-  //   const getData = async () => {
-  //     try {
-  //       const result = await fetchExampleData();
-  //       setData(result);
-  //     } catch (error) {
-  //       console.error('Error fetching data:', error);
-  //     }
-  //   };
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const result = await fetchExampleData();
+        setData(result);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
 
-  //   getData();
-  // }, []);
+    getData();
+  }, []);
 
   return (
     <div>
       <h1>Example View</h1>
-      {/* {data ? <pre>{JSON.stringify(data, null, 2)}</pre> : <p>Loading...</p>} */}
+      {data ? <pre>{JSON.stringify(data, null, 2)}</pre> : <p>Loading...</p>}
     </div>
   );
 };
