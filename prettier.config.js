f7aef885d183521b24c2e746f616452e842cecbf
DwiWahyu20 <146156027+DwiWahyu20@users.noreply.github.com>
2025-03-18 15:05:49 +0000

    reset

--- prettier.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import("prettier").Config} */
-module.exports = {
-  semi: false,
-  singleQuote: true,
-  jsxSingleQuote: true,
-  arrowParens: 'avoid',
-  plugins: ['prettier-plugin-tailwindcss'],
-}
