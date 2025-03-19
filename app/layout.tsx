72caffeb2a3fa950ba62502d419d58bc40ae67e1
DwiWahyu20 <146156027+DwiWahyu20@users.noreply.github.com>
2025-03-19 06:05:21 +0000

    gsg

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,9 +3,12 @@ import './globals.css';
 import { Analytics } from '@vercel/analytics/react';
 
 export const metadata = {
-  title: 'Next.js App Router + NextAuth + Tailwind CSS',
+  // title: 'Next.js App Router + NextAuth + Tailwind CSS',
+  // description:
+  // 'A user admin dashboard configured with Next.js, Postgres, NextAuth, Tailwind CSS, TypeScript, and Prettier.'
+  title: 'Project Deucalion Dashboard',
   description:
-    'A user admin dashboard configured with Next.js, Postgres, NextAuth, Tailwind CSS, TypeScript, and Prettier.'
+    'React Experiment'
 };
 
 export default function RootLayout({
