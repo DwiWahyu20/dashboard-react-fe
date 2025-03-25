ebeb313d13323b9e4a9236010171a801073d9a4a
DwiWahyu20 <146156027+DwiWahyu20@users.noreply.github.com>
2025-03-25 04:48:56 +0000

    daadas

--- app/(controllers)/controller.ts
+++ app/(controllers)/controller.ts
@@ -1,10 +1,14 @@
 
 import { fetchData, postData } from 'app/(services)/apiService';
 
-export const forwardResponse = async () => {
+interface ControllerResponse<T> {
+  data: T;
+}
+
+export const forwardResponse = async <T>(): Promise<T> => {
     try {
-        const response = await fetchData('/cors');
-        return response;
+        const response: ControllerResponse<T> = await fetchData('/cors');
+        return response.data;
     } catch (error) {
         console.error('Error fetching data:', error);
         throw error;
